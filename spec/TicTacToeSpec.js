5cc01dbd47e8007c9292b4f5e5adfbe2af51f1ec
Juan Manuel Vicente <juanma.v82@gmail.com>
2016-12-28 12:50:43 -0300

    Update unit test to use Tape.js

--- spec/TicTacToeSpec.js
+++ spec/TicTacToeSpec.js
@@ -1,83 +1,93 @@
-describe("Game", function() {
 
-    var TicTacToe = require('../TicTacToe');
-    var tictactoe;
-
-    beforeEach(function() {
-        tictactoe = new TicTacToe();
-    });
-
-    it("is over when all fields are taken", function() {
-        tictactoe.board =
-            [
-                1,2,1,
-                1,2,1,
-                2,1,1
-            ];
-
-        expect(tictactoe.allPositionTaken()).toBe(true);
-    });//*/
-
-    it("is over when all fields in a column are taken by a player", function() {
-        tictactoe.board =
-            [
-                0,0,1,
-                0,0,1,
-                0,0,1
-            ];
-
-        expect(tictactoe.columnIsTakenByAPlayer()).toBe(true);
-    });//*/
-
-    it("is over when all fields in a row are taken by a player", function() {
-        tictactoe.board =
-            [
-                1,1,1,
-                0,0,0,
-                0,0,0
-            ];
-
-        expect(tictactoe.rowIsTakenByAPlayer()).toBe(true);
-    });//*/
-
-    it("is over when all fields in a diagonal are taken by a player", function() {
-        tictactoe.board =
-            [
-                0,0,1,
-                0,1,0,
-                1,0,0
-            ];
-
-        expect(tictactoe.diagonalIsTakenByAPlayer()).toBe(true);
-    });//*/
-
-    it("player can take a field if not already taken", function() {
-        expect(tictactoe.playerTakePosition(8)).toBe(true);
-    });//*/
+const test = require('tape');
+const TicTacToe = require('../TicTacToe');
 
 
-    // Esta prueba me va hacer modificar el metodo desarrollado anteriormente "playerTakePosition"
-    it("players take turns taking fields until the game is over", function() {
-        expect(tictactoe.playerTakePosition(8)).toBe(true);
-        expect(tictactoe.playerTakePosition(1)).toBe(true);
-        expect(tictactoe.playerTakePosition(2)).toBe(true);
-        expect(tictactoe.playerTakePosition(6)).toBe(true);
-        expect(tictactoe.playerTakePosition(7)).toBe(true);
-        expect(tictactoe.playerTakePosition(4)).toBe(true);
-        expect(tictactoe.playerTakePosition(5)).toBe(true);
-        expect(tictactoe.playerTakePosition(0)).toBe(false);
+    var tictactoe = new TicTacToe();
 
-    });//*/
+test("is over when all fields are taken", function(t) {
+    var tictactoe = new TicTacToe();
+    tictactoe.board =
+        [
+            1,2,1,
+            1,2,1,
+            2,1,1
+        ];
 
-    // Test Auxiliar para verificar si alguien gano despues de una jugada.
-    it("Verify if someone won in a given board", function(){
-        tictactoe.board =
-            [
-                2,2,1,
-                1,1,2,
-                1,2,2
-            ];
-        expect(tictactoe.someoneWin()).toBe(true);
-    });//*/
+    t.true(tictactoe.allPositionTaken());
+    t.end();
+});//*/
 
-});
+test("is over when all fields in a column are taken by a player", function(t) {
+    var tictactoe = new TicTacToe();
+    tictactoe.board =
+        [
+            0,0,1,
+            0,0,1,
+            0,0,1
+        ];
+
+    t.true(tictactoe.columnIsTakenByAPlayer());
+    t.end();
+});//*/
+
+test("is over when all fields in a row are taken by a player", function(t) {
+    var tictactoe = new TicTacToe();
+    tictactoe.board =
+        [
+            1,1,1,
+            0,0,0,
+            0,0,0
+        ];
+
+    t.true(tictactoe.rowIsTakenByAPlayer());
+    t.end();
+});//*/
+
+test("is over when all fields in a diagonal are taken by a player", function(t) {
+    var tictactoe = new TicTacToe();
+    tictactoe.board =
+        [
+            0,0,1,
+            0,1,0,
+            1,0,0
+        ];
+
+    t.true(tictactoe.diagonalIsTakenByAPlayer());
+    t.end();
+});//*/
+
+test("player can take a field if not already taken", function(t) {
+    var tictactoe = new TicTacToe();
+    t.true(tictactoe.playerTakePosition(8));
+    t.end();
+});//*/
+
+
+// Esta prueba me va hacer modificar el metodo desarrollado anteriormente "playerTakePosition"
+test("players take turns taking fields until the game is over", function(t) {
+    var tictactoe = new TicTacToe();
+    t.true(tictactoe.playerTakePosition(8));
+    t.true(tictactoe.playerTakePosition(1));
+    t.true(tictactoe.playerTakePosition(2));
+    t.true(tictactoe.playerTakePosition(6));
+    t.true(tictactoe.playerTakePosition(7));
+    t.true(tictactoe.playerTakePosition(4));
+    t.true(tictactoe.playerTakePosition(5));
+    t.false(tictactoe.playerTakePosition(0));
+    t.end();
+});//*/
+
+// Test Auxiliar para verificar si alguien gano despues de una jugada.
+test("Verify if someone won in a given board", function(t){
+    var tictactoe = new TicTacToe();
+    tictactoe.board =
+        [
+            2,2,1,
+            1,1,2,
+            1,2,2
+        ];
+    t.true(tictactoe.someoneWin());
+    t.end();
+});//*/
+
